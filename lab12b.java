7b91ed9986db4a702d5b29403133df1e9a8048fe
Sandeep <69573725+SandeepMullangi@users.noreply.github.com>
2020-12-02 12:11:23 +0530

    Add files via upload
    Call by Reference

---
+++ lab12b.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.io.*;
+class cbr
+{
+int a,b;
+	 void read(cbr c)
+	{
+	c.a=c.a+10;
+        c.b=c.b+10;
+	}
+}
+class lab12b
+{
+public static void main(String... ar)
+{
+cbr c=new cbr();
+Scanner s=new Scanner(System.in);
+System.out.println("a value");
+c.a=s.nextInt();
+System.out.println("b value");
+c.b=s.nextInt();
+System.out.println("before the a and b values are"+c.a+","+c.b);
+c.read(c);
+System.out.println("after the a and  b values are"+c.a+","+c.b);
+}
+}
